5b0329d6441265c2243a9906627fa913040eadc9
shinbumKang <tlsqja1230@naver.com>
2020-06-11 22:13:06 +0900

    주석 추가

--- components/BundangguMap.js
+++ components/BundangguMap.js
@@ -26,22 +26,29 @@ export default class BundangguMap {
   }
 
   render() {
+    // 해당 위도,경도로 위치이동
     this.map.setCenter(new kakao.maps.LatLng(37.4020288, 127.1066516));
+
+    // 마커 이미지 객체 생성
     const markerImage = new kakao.maps.MarkerImage(
       "https://grepp-cloudfront.s3.ap-northeast-2.amazonaws.com/programmers_imgs/competition-imgs/2020_kakao_challenge/marker-green.png",
       new kakao.maps.Size(24, 34)
     );
+
+    // 마커 객체 생성
     const marker = new kakao.maps.Marker({
       position: new kakao.maps.LatLng(37.4020288, 127.1066516),
       title: "title",
       image: markerImage,
     });
 
+    // 지도에 마커 그리기.
     marker.setMap(this.map);   
 
     // 마커 숨기기
     // marker.setMap(null)
 
+    // 정보창 생성
     var infoWindow = new kakao.maps.InfoWindow({
       map: this.map,
       position: new kakao.maps.LatLng(37.3947464,127.1090181,17),
